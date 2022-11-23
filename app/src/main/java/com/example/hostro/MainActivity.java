ec12237019d7c691ef8e0a9357d52d4d9817a205
Yash Gangamwar <gangamwaryash@gmail.com>
2022-11-23 18:15:54 +0530

    Android

--- app/src/main/java/com/example/hostro/MainActivity.java
+++ app/src/main/java/com/example/hostro/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.hostro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b = findViewById(R.id.button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "I am Yash", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
